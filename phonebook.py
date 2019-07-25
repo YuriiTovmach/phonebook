7bf061650ab840e474c462423e57be811d2375d9
YuriiTovmach <portfolio_yurii_tovmach@ukr.net>
2019-07-25 15:05:12 +0300

    third commit

--- phonebook.py
+++ phonebook.py
@@ -1,21 +1,33 @@
 # phonebook
 
-users = {   "user_1" : "+3 8 098 456 - 71 -23",
-            "user_2" : "+3 8 098 456 - 72 -23",
-            "user_3" : "+3 8 098 456 - 73 -23",
-            "user_4" : "+3 8 098 456 - 74 -23",
-            "user_5" : "+3 8 098 456 - 75 -23",
-            "user_6" : "+3 8 098 456 - 76 -23",
-            "user_7" : "+3 8 098 456 - 77 -23",
-            "user_8" : "+3 8 098 456 - 78 -23",
-            "user_9" : "+3 8 098 456 - 79 -23",
-            "user_10" : "+3 8 098 456 - 80 -23"
+
+# base
+users = {   "user_1" : "80984567123",
+            "user_2" : "80984567124",
+            "user_3" : "80984567125",
+            "user_4" : "80984567126",
+            "user_5" : "80984567127",
+            "user_6" : "80984567128",
+            "user_7" : "80984567129",
+            "user_8" : "80984567130",
+            "user_9" : "80984567131",
+            "user_10" : "80984567132"
 }
+
+
+
+# add new phones to base
+new_user = str(input("write down new_user_name:"))
+# if new_user == True:
+#     users.apdate([new_user])
+
+new_phone = int(input("write down new_user_phone:"))
+# if new_phone == True:
+#     users.apdate([new_phone])
+
+
+
+users.update({new_user : new_phone})
 
 for key in users:
     print(key, " - ", users[key])
-
-
-
-
-
